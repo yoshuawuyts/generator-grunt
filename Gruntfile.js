a00a189836c7a43cc627d0c45a76b2ec8227f04b
Yoshua Wuyts <yoshuawuyts@gmail.com>
2013-12-08 17:19:52 +0100

    add gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+  grunt.initConfig({
+  });
+
+  // Load all grunt tasks
+  require('load-grunt-tasks')(grunt);
+  require('time-grunt')(grunt);
+};
